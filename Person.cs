0fde02197c70be3932872401ae17e90d7d3daa33
robotevan <robotevan24@gmail.com>
2020-01-12 11:04:21 -0500

    fixing json parser

--- Person.cs
+++ Person.cs
@@ -1,13 +1,20 @@
 namespace CuHackingMurder
 {
+    /// <summary>
+    /// Class for a Person
+    /// </summary>
     class Person
     {
         private string name;
-
+        /// <summary>
+        /// Constructor for Person
+        /// @param string name
+        /// </summary>
+        /// <param name="name"></param>
         public Person(string name){
             this.name = name;
         }
-
+        //return the name of the person
         public string getName(){
             return name;
         }
